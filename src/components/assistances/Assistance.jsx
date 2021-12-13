d854b8b72abdb13ba83b718f5cdd3f1c3de00c3b
Josué Munganga <jmunganga039@gmail.com>
2021-12-13 11:27:59 +0200

    Worked on the assistance component

--- src/components/assistances/Assistance.jsx
+++ src/components/assistances/Assistance.jsx
@@ -1,9 +1,48 @@
+import { useEffect, useRef } from 'react';
+import { init } from 'ityped';
 import './assistance.scss'
 
 const Assistance = () => {
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: ["Questions Importantes", "Problèmes Techniques", "Difficultés Rencontrées"]
+    })
+  }, []);
+
   return (
-    <div>
-      Assistance
+    <div className="assistance">
+      <div className="container">
+        <div className="row">
+          <div className="col-8">
+            <h2 className="h2">
+              Assisitance 24/7 pour vos <br />
+              <span className="dynamic-text" ref={textRef}></span>
+            </h2>
+            <p className="lead my-4">
+              Notre support client est actif 24 heures sur 7 jours pour vous assister.
+              Une équipe déterminée pour vous apporter les solutions à tout problème 
+              lié avec votre compte. Nous sommes très poli et très courtois envers nos clients
+            </p>
+            <div className="action-buttons my-4">
+              <button className="btn btn-lg btn-info">
+                Trouver la Solution <i className="fas fa-arrow-right"></i>
+              </button>
+              <button className="btn btn-lg btn-light mx-3">
+                Contacter Fyatu <i className="fa fa-phone"></i>
+              </button>
+            </div>
+            
+          </div>
+          <div className="col-4">
+            <img src="https://sandbox.fyatu.com/assets/img/support.png" alt="" className="img-fluid"/>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
