c57224e5833ce18f2effde35d1840e2934c789ce
Josué Munganga <jmunganga039@gmail.com>
2021-12-22 13:23:31 +0200

    Worked on the advantages responsiveness

--- src/components/advantages/Advantages.jsx
+++ src/components/advantages/Advantages.jsx
@@ -4,26 +4,30 @@ import './advantages.scss'
 
 const Advantages = () => {
   return (
-    <div className="advantages">
+    <section className="slice slice-lg bg-section-light">
       <div className="container">
-        <h2 className="h2 my-4">Une seule application, plusieurs avantages !</h2>
-        <p className="lead">
-          FYATU est au centre de plusieurs fonctionnalités vous aidant ainsi de vous retrouver dans l'une ou l'autre afin de développer votre propre Business autour de notre noyau.
-        </p>
+        <div className="row mb-5 justify-content-center text-center">
+          <div className="col-lg-12">
+            <h2 className="h1 text-black my-4">Une seule application, plusieurs avantages !</h2>
+            <p className="lead lh-200 text-black">
+              FYATU est au centre de plusieurs fonctionnalités vous aidant ainsi de vous retrouver dans l'une ou l'autre afin de développer votre propre Business autour de notre noyau.
+            </p>
+          </div>
+        </div>
         <div className="row">
           {advantages.map(({id, img, title, desc}) => (
-            <div key={id} className="col-6">
-              <div className="details-card row">
-                <div className="col-3">
-                  <img src={img} alt="" className="img-fluid"/>
-                </div>
-                <div className="col-9">
-                  <h3 className="h3">
-                    {title}
-                  </h3>
-                  <p className="card-text">
-                    {desc}
-                  </p>
+            <div key={id} className="col-lg-6 col-12 col-sm-12">
+              <div className="card text-center hover-translate-y-n10 hover-shadow-lg">
+                <div className="row px-4 pb-4 pt-4">
+                  <div className="col-3 text-center">
+                    <img src={img} alt="" className="img-fluid"/>
+                  </div>
+                  <div className="col-9 text-left">
+                    <h5>{title}</h5>
+                    <p className="mt-2 mb-0">
+                      {desc}
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
@@ -31,7 +35,7 @@ const Advantages = () => {
         </div>
 
       </div>
-    </div>
+    </section>
   )
 }
 
--- src/components/announcement/Announcement.jsx
+++ src/components/announcement/Announcement.jsx
@@ -2,24 +2,26 @@ import './announcement.scss'
 
 const Announcement = () => {
   return (
-    <div className="announcement">
+    <section className="slice bg-white">
       <div className="container">
         <div className="row">
-          <div className="col">
-            <div className="imgContainer">
-              <img src="https://sandbox.fyatu.com/assets/img/methods.png" alt="" className="img-fluid"/>
-            </div>
+          <div className="col-lg-7 col-12 p-2">
+            <img class='coins-img p-4' alt="" src="https://sandbox.fyatu.com/assets/img/methods.png" width='100%' />
           </div>
-          <div className="col">
-            <h2 className="h2">Les modes de <br /> paiement locaux et internationaux. </h2>
-            <p className="lead"> Fyatu vous offre la possibilité d'alimenter votre compte ou de retirer l'argent dans votre compte via les modes de paiements locaux dans 14 pays et d'autres modes de paiements internationaux pour les restes des pays du monde.</p>
-            <button className="btn btn-demo btn-lg btn-info">
+          <div className="col-lg-5 col-12">
+            <h1 className="text-black pt-4">
+              Les modes de paiement locaux et internationaux. 
+            </h1>
+            <p className="mt-2 text-black">
+              Fyatu vous offre la possibilité d'alimenter votre compte ou de retirer l'argent dans votre compte via les modes de paiements locaux dans 14 pays et d'autres modes de paiements internationaux pour les restes des pays du monde.
+            </p>
+            <button className="btn btn-primary mt-4">
               Demander une Démonstration <i className="fa fa-download"></i>
             </button>
           </div>
         </div>
       </div>
-    </div>
+    </section>
   )
 }
 
