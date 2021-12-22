9ad02508ba8c4dfd2fa7a20961b2b0e7b4c3c166
Josué Munganga <jmunganga039@gmail.com>
2021-12-22 12:56:42 +0200

    Responsiveness whychooseus section

--- src/components/whychooseus/WhyChooseUs.jsx
+++ src/components/whychooseus/WhyChooseUs.jsx
@@ -1,37 +1,41 @@
-import './whychooseus.scss'
 import { whyUseUs } from '../../data';
 
 
 const WhyChoosUs = () => {
   return (
-    <div className="why">
-
-      <div className="container">
-        <h1 className="display-1">
-          Pourquoi +30,O00 clients nous font confiance ?
-        </h1>
-        <p className="lead">
-          Fyatu facilite l'achat des cartes virtuelles, la recharge et le transfert d'argent.
-        </p>
+    <section className="why slice bg-section-light">
+      <div className="container position-relative zindex-100 mt-4">
+        <div className="row mb-5 justify-content-center text-center">
+          <div className="col-lg-12">
+            <h6 className="h1 text-black">
+              Pourquoi +30,O00 clients nous font confiance ?
+            </h6>
+            <div className="mt-2">
+              <p className="lead lh-200 text-black">
+                Fyatu facilite l'achat des cartes virtuelles, la recharge et le transfert d'argent.
+              </p>
+            </div>
+          </div>
+        </div>
         <div className="row">
           {whyUseUs.map(({ id, img, title, desc }) => (
-            <div className="col">
-              <div className="card text-center">
-                <div className="card-body">
+            <div className="col-lg-4 col-12 mb-5 mb-lg-0">
+              <div className="text-center">
+                <div className="mb-4">
                   <img src={img} alt=""/>
-                  <h3 className="card-title">
-                    {title}
-                  </h3>
-                  <p className="card-text">
-                    {desc}
-                  </p>
                 </div>
+                <div className="h3 text-black">
+                  <span className="counter-extra">{title}</span>
+                </div>
+                <p className="lead lh-180 text-black">
+                  {desc}
+                </p>
               </div>
             </div>
           ))}
         </div>
       </div>
-    </div>
+    </section>
   )
 }
 
